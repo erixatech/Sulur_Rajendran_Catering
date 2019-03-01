bd07759655d4dee762f66c3b89e6ff9d4d9fd693
erixatech <erixatech@gmail.com>
2019-03-02 02:53:45 +0530

    Changes in Spark

--- server/spark.js
+++ server/spark.js
@@ -1,27 +1,17 @@
-var fs = require('fs');
 var express = require("express");
 var app = express();
 var router = express.Router();
-//var path = __dirname + '/views/'; // this folder should contain your html files.
-var mongoOpn1 = require('./dbHandler');
+
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var http = require('http');
-var fs = require('fs');
-var jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-global.$;
-global.a = 'a';
+var mongoOpn1 = require('./dbHandler');
 
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.use(bodyParser.json()); // for parsing application/json
 
 router.post("/createIngredient", function(req, res){  
-	/*var testJsonOld = {"name" : "Guru"};
-	var testJson = {"name" : "Arun"};
-  console.log(req.body);*/
 	mongoOpn1.mongoOpns(req, res, "Ingredients", "insert", req.body);
 });
 
