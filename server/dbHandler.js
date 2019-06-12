0b6cf8118065ea61df52deb9538db41b20de8910
erixatech <erixatech@gmail.com>
2019-06-13 00:59:15 +0530

    Connect Remote Mongo DB

--- server/dbHandler.js
+++ server/dbHandler.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+//var url = "mongodb://localhost:27017/";
+var url = "mongodb+srv://erixatech:hailerixa@erixacluster-xlmmj.mongodb.net/test?retryWrites=true&w=majority";
 var dbName = "src";
 
 function mongoOpns(req, res, collectionName, operation, dataJson, query)
